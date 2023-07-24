ae2a858c77219e4e494cd01701219b3e2cbe1a7f
bbordere <bastien.bordereau@gmail.com>
2023-07-24 06:30:57 -0400

    tweaks

--- Makefile
+++ Makefile
@@ -2,11 +2,15 @@ DATA_PATH = "/home/bbordere/data"
 
 all:
 	mkdir -p $(DATA_PATH)/mariadb $(DATA_PATH)/wordpress
-	docker compose -f srcs/docker-compose.yml up --build
+	docker compose -f srcs/docker-compose.yml up --build -d
 
 stop:
 	docker compose -f srcs/docker-compose.yml stop
-	docker compose -f srcs/docker-compose.yml down
 
 clean: stop
+	docker compose -f srcs/docker-compose.yml down
 	sudo rm -Rf $(DATA_PATH)/wordpress/* $(DATA_PATH)/mariadb/*
+
+re: clean all
+
+.PHONY: all stop clean re
--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -2,7 +2,7 @@
 
 /wait
 
-echo "127.0.0.1 bbordere.42.fr" >> /etc/hosts
+# echo "127.0.0.1 bbordere.42.fr" >> /etc/hosts
 
 if ! wp --allow-root core is-installed; then
 
@@ -12,15 +12,13 @@ if ! wp --allow-root core is-installed; then
 									--dbpass=$DB_USER_PWD --dbhost=mariadb --dbprefix=wp_
 
 	wp --allow-root core install 													\
-				--url=https://localhost --title=Inception 					\
+				--url=https://bbordere.42.fr --title=Inception 					\
 				--admin_user=$WP_USER_ADMIN --admin_password=$WP_USER_ADMIN_PWD \
 				--admin_email=$WP_USER_ADMIN_MAIL --skip-email
 
 	wp --allow-root user create $WP_USER_AUTHOR $WP_USER_AUTHOR_MAIL --user_pass=$WP_USER_AUTHOR_PWD --role=contributor
 
-	# wp --allow-root theme install north-shore --activate
 	wp --allow-root theme install lensation --activate
-# rosa-lite
 fi
 
 php-fpm7 -F
